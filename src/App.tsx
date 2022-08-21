73210f9008e1dc9e18685ec944bdf0ba7f07f72f
Natalia <natalia.araujo@yazo.com.br>
2022-08-21 20:04:22 -0300

    chore: exemplo de pro de cor

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { Button } from "./components/Button";
 export function App() {
   return (
     <>
-      <Button color="danger" />
-      <Button color="primary" />
-      <Button color="secundary" />
-      <Button color="sucess" />
-      <Button color="warning" />
-      <Button />
+      <Button variant="primary" />
+      <Button variant="secundary" />
+      <Button variant="sucess" />
+      <Button variant="warning" />
     </>
   );
 }
--- src/components/button.styles.ts
+++ src/components/button.styles.ts
@@ -9,11 +9,21 @@ export type ButtonVariant =
 interface ButtonContainerProps {
   variant: ButtonVariant;
 }
-
+const buttonVariants = {
+  primary: "#100018",
+  secundary: "#ff8a14",
+  danger: "#a30f01",
+  sucess: "#18ba64",
+  warning: "#e6d406",
+};
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 171px;
   height: 56px;
   border: 4px;
   margin: 2rem;
   padding: 16px, 48px, 16px, 48px;
+
+  ${(props) => {
+    return `background-color: ${buttonVariants[props.variant]}`;
+  }}
 `;
